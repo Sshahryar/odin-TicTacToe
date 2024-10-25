763dcedeb812432df209ceb85eaca5aaa2680bb0
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-10-25 00:16:55 -0400

    added game controller

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ const Gameboard = (function() {
 const Player = (name, marker) => {
     return { name, marker };
 };
+
+const GameController = (function() {
+    const playerX = Player('Player X', 'X');
+    const playerO = Player('Player O', 'O');
+    let currentPlayer = playerX;
+
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === playerX ? playerO : playerX;
+    };
+
+    const makeMove = (index) => {
+        if (Gameboard.getBoard()[index] === '') {
+            Gameboard.setField(index, currentPlayer.marker);
+            switchPlayer();
+        }
+    };
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    return { makeMove, getCurrentPlayer };
+})();
