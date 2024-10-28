70040f0a4e4294a01a1fad9ff991d50314d765b0
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-10-27 23:07:00 -0400

    fullly implemented logic
    testing a sample game

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const Gameboard = (function() {
     const board = ['', '', '', '', '', '', '', '', ''];
-    
+
     const getBoard = () => board;
     const setField = (index, value) => { board[index] = value; };
-
     return { getBoard, setField };
 })();
 
@@ -23,11 +22,42 @@ const GameController = (function() {
     const makeMove = (index) => {
         if (Gameboard.getBoard()[index] === '') {
             Gameboard.setField(index, currentPlayer.marker);
-            switchPlayer();
+            if (checkWinner()) {
+                console.log(`${currentPlayer.name} wins!`);
+            } else if (checkTie()) {
+                console.log("It's a tie!");
+            } else {
+                switchPlayer();
+            }
         }
     };
 
-    const getCurrentPlayer = () => currentPlayer;
+    const checkWinner = () => {
+        const board = Gameboard.getBoard();
+        const winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+        return winPatterns.some(pattern => {
+            const [a, b, c] = pattern;
+            return board[a] && board[a] === board[b] && board[a] === board[c];
+        });
+    };
 
+    const checkTie = () => {
+        return Gameboard.getBoard().every(cell => cell !== '');
+    };
+
+    const getCurrentPlayer = () => currentPlayer;
     return { makeMove, getCurrentPlayer };
 })();
+
+//Sample test game
+GameController.makeMove(0);
+GameController.makeMove(1);
+GameController.makeMove(4);
+GameController.makeMove(2);
+GameController.makeMove(8);
+
+
